2c0ae0efa7432804e88e37d7868250884de7c420
aaronius <aaroniusv@gmail.com>
2015-10-29 08:36:54 +0000

    working away

--- characterhelper.js
+++ characterhelper.js
@@ -1,15 +1,21 @@
 Characters = new Mongo.Collection("characters");
 
 if (Meteor.isClient) {
-  if (auth)
-  
-  {
-    Template.main.helpers({
-    characters: function() {
-      return Characters.find();
+  // This code only runs on the client
+  Template.body.helpers({
+    characters: function()
+    {
+      return
     }
-    
-  })  
-  }
-  
+  });
+}
+
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Characters.find({});
+    }
+  });
 }
