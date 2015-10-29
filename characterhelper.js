d3359e29b999ded86840f16e7531acb8b13d5fd7
aaronius <colormelt@gmail.com>
2015-10-29 09:08:22 +0000

    added remove button

--- characterhelper.js
+++ characterhelper.js
@@ -3,19 +3,51 @@ Characters = new Mongo.Collection("characters");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    characters: function()
-    {
-      return
-    }
-  });
-}
-
- 
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
+    characters: function () {
       return Characters.find({});
     }
   });
-}
+  
+  Template.body.events({
+    "submit .new-character": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Characters.insert({
+        text: text,
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+  
+  Template.character.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Characters.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Characters.remove(this._id);
+    }
+  });
+  
+  
+  
+  
+
+  
+} // is client
+  
+  
+  
+  
+  
+
+ 
